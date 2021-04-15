5f2d75b879b002a86e721316cd50678f89c8729c
Алексей Коржавин <hughwols@gmail.com>
2021-04-15 18:43:32 +0200

    navigator service for using routing outside from widgets

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,14 @@
+import 'package:cxl/services/navigator_service.dart';
 import 'package:cxl/ui/screens/home.dart';
 import 'package:flutter/material.dart';
+
+import 'dependency_injection.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: getIt<NavigatorService>().navigatorKey,
       home: HomeScreen(
         title: 'Flutter Demo Home Page',
       ),
---
+++ lib/services/navigator_service.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+
+@lazySingleton
+class NavigatorService {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  Future<dynamic> push(String routeName, {dynamic arguments}) {
+    return navigatorKey.currentState!
+        .pushNamed(routeName, arguments: arguments);
+  }
+
+  Future<dynamic> pushReplacementNamed(String routeName, {dynamic args}) {
+    return navigatorKey.currentState!
+        .pushReplacementNamed(routeName, arguments: args);
+  }
+
+  void pop() {
+    return navigatorKey.currentState!.pop();
+  }
+}
